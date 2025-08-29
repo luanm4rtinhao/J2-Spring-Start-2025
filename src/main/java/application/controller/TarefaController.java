6d8f1dcb8d79c629dd09a1b44af476a9b9262bcb
luanm4rtinhao <luanmartinhao10@gmail.com>
2025-08-29 19:25:21 +0000

    Aula-29/08/2025

--- src/main/java/application/controller/TarefaController.java
+++ src/main/java/application/controller/TarefaController.java
@@ -6,6 +6,8 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import application.model.Tarefa;
 
@@ -16,12 +18,19 @@ public class TarefaController {
 
     @RequestMapping("/list")
     public String list(Model ui) {
-        tarefas.add(new Tarefa(1,"Aprender java"));
-        tarefas.add(new Tarefa(2,"Estudar IoT"));
+        //tarefas.add(new Tarefa(1,"Aprender java"));
+       //tarefas.add(new Tarefa(2,"Estudar IoT"));
 
 
         ui.addAttribute("tarefas", tarefas);
         return "list";
     }
+    
+    @RequestMapping(value = "/insert", method = RequestMethod.POST)
+    public String add(@RequestParam("tarefa") String nomeTarefa) {  
+        tarefas.add(new Tarefa(0, nomeTarefa));
+        return "redirect:/tarefas/list";
+    }
 }
+
 
